75be51d39551ecf3aba5a4644311972ae8bf41f3
Christopher Hair <mystikweb@live.ca>
2023-05-15 17:39:55 -0400

    Making sure shared lib works

--- apps/modulea/src/app/remote-entry/entry.component.ts
+++ apps/modulea/src/app/remote-entry/entry.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { UserService } from '@ng-mf/shared';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ng-mf-modulea-entry',
-  template: `<h1>Module A Home</h1>`,
+  template: `
+    <h1>Module A Home</h1>
+    <div>User is {{ (authenticated$ | async) ? 'authenticated': 'not authenticated' }}</div>
+  `,
 })
-export class RemoteEntryComponent {}
+export class RemoteEntryComponent {
+  private _userService = inject(UserService);
+
+  get authenticated$(): Observable<boolean> { return this._userService.isUserLoggedIn$; }
+}
